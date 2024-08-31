1178699dc9a25a865ba43cb8baf25a505bf6e8e8
gazalkagdi <gazalkagdi111@gmail.com>
2024-08-31 20:54:37 +0530

    deployment changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -79,7 +79,7 @@ export const Navbar = () => {
             {isAuthenticated && isSubscribed && (
               <Link
                 rel="noreferrer noopener"
-                href="#"
+                href={process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL!}
                 target="_blank"
                 className={`text-[17px] ${buttonVariants({
                   variant: "ghost",
